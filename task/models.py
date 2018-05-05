0035daa36bc67b79451b46592670f9e07ecef63a
Andrei Avram <andrei.avram@spyhce.com>
2018-05-05 17:22:01 +0300

    first commit

---
+++ task/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import CASCADE
+
+
+class TaskItem(models.Model):
+    title = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    owner = models.ForeignKey("auth.User", models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
